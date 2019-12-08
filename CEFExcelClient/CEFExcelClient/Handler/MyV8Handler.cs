9fd425e1cf7c6aa856c1dbed695465ae462d920a
Rex <im@rex.tw>
2019-12-08 23:22:41 +0800

    研究中 http://lingyuncxb.com/2018/05/26/CEFGlue%E4%B9%8B%E5%9B%9E%E5%BF%86%E7%AF%87/

---
+++ CEFExcelClient/CEFExcelClient/Handler/MyV8Handler.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Xilium.CefGlue;
+
+namespace CEFExcelClient.Handler
+{
+    //http://lingyuncxb.com/2018/05/26/CEFGlue%E4%B9%8B%E5%9B%9E%E5%BF%86%E7%AF%87/
+    public class MyV8Handler : CefV8Handler
+    {
+        public MyV8Handler()
+        {
+            this.name = "lingyun";
+        }
+
+        private string name;
+        public string Name
+        {
+            set { this.name = value; }
+            get { return this.name; }
+        }
+
+        public void MyFunction()
+        {
+            MessageBox.Show("JS调用C# : MyFunction");
+        }
+
+        private string email;
+        public string GetEmail()
+        {
+            return this.email;
+        }
+        public void SetEmail(string email)
+        {
+            this.email = email;
+        }
+
+        protected override bool Execute(string name, CefV8Value obj, CefV8Value[] arguments, out CefV8Value returnValue, out string exception)
+        {
+            string result = string.Empty;
+            switch (name)
+            {
+                case "MyFunction":
+                    MyFunction();
+                    break;
+
+                case "GetEmail":
+                    result = GetEmail();
+                    break;
+
+                case "SetEmail":
+                    SetEmail(arguments[0].GetStringValue());
+                    break;
+
+                default:
+                    break;
+            }
+
+            returnValue = CefV8Value.CreateString(result);
+            exception = null;
+            return true;
+        }
+    }
+}
---
+++ CEFExcelClient/CEFExcelClient/ProcessHandler/WpfRenderProcessHandler.cs
@@ -0,0 +1,59 @@
+﻿using CEFExcelClient.Handler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xilium.CefGlue;
+
+namespace CEFExcelClient.ProcessHandler
+{
+    //http://lingyuncxb.com/2018/05/26/CEFGlue%E4%B9%8B%E5%9B%9E%E5%BF%86%E7%AF%87/
+    class WpfRenderProcessHandler : CefRenderProcessHandler
+    {
+        private MyV8Handler myHandle;
+        protected override void OnWebKitInitialized()
+        {
+            myHandle = new MyV8Handler();
+
+            const string jsCode = @"function myHandle() {}
+
+        if (!myHandle) myHandle = {};
+
+        (function() {
+
+            myHandle.__defineGetter__('name',
+            function() {
+                native function GetName();
+                return GetName();
+            });
+
+            myHandle.__defineSetter__('name',
+            function(arg0) {
+                native function SetName(arg0);
+                SetName(arg0);
+            });
+
+            myHandle.myFunction = function() {
+                native function MyFunction();
+                return MyFunction();
+            };
+
+            myHandle.getEmail = function() {
+                native function GetEmail();
+                return GetEmail();
+            };
+
+            myHandle.setEmail = function(arg0) {
+                native function SetEmail(arg0);
+                SetEmail(arg0);
+            };
+
+        })();";
+
+            CefRuntime.RegisterExtension("myHandleName", jsCode, myHandle);
+
+            base.OnWebKitInitialized();
+        }
+    }
+}
