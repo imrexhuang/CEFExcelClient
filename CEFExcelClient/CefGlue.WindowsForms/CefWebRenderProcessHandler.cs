efba1ad4b1348ddded95b27650eb9dac10651bce
Rex <im@rex.tw>
2019-12-08 18:04:55 +0800

    調整程式碼

--- CEFExcelClient/CefGlue.WindowsForms/CefWebRenderProcessHandler.cs
+++ CEFExcelClient/CefGlue.WindowsForms/CefWebRenderProcessHandler.cs
@@ -6,7 +6,66 @@ using System.Threading.Tasks;
 
 namespace Xilium.CefGlue.WindowsForms
 {
-    class CefWebRenderProcessHandler
+    /* BEG: modbyme */
+    internal sealed class CefWebRenderProcessHandler : CefRenderProcessHandler
     {
+        private readonly CefWebBrowser _core;
+
+        public CefWebRenderProcessHandler(CefWebBrowser core)
+        {
+            _core = core;
+        }
+
+        protected override CefLoadHandler GetLoadHandler()
+        {
+            return base.GetLoadHandler();
+        }
+
+        //protected override void OnBrowserCreated(CefBrowser browser)
+        //{
+        //    base.OnBrowserCreated(browser);
+
+        //}
+
+        protected override void OnBrowserDestroyed(CefBrowser browser)
+        {
+            base.OnBrowserDestroyed(browser);
+        }
+
+        protected override void OnContextCreated(CefBrowser browser, CefFrame frame, CefV8Context context)
+        {
+            base.OnContextCreated(browser, frame, context);
+        }
+
+        protected override void OnContextReleased(CefBrowser browser, CefFrame frame, CefV8Context context)
+        {
+            base.OnContextReleased(browser, frame, context);
+        }
+
+        protected override void OnFocusedNodeChanged(CefBrowser browser, CefFrame frame, CefDomNode node)
+        {
+            base.OnFocusedNodeChanged(browser, frame, node);
+        }
+
+        //protected override bool OnProcessMessageReceived(CefBrowser browser, CefProcessId sourceProcess, CefProcessMessage message)
+        //{
+        //    return base.OnProcessMessageReceived(browser, sourceProcess, message);
+        //}
+
+        protected override void OnRenderThreadCreated(CefListValue extraInfo)
+        {
+            base.OnRenderThreadCreated(extraInfo);
+        }
+
+        protected override void OnUncaughtException(CefBrowser browser, CefFrame frame, CefV8Context context, CefV8Exception exception, CefV8StackTrace stackTrace)
+        {
+            base.OnUncaughtException(browser, frame, context, exception, stackTrace);
+        }
+
+        protected override void OnWebKitInitialized()
+        {
+            base.OnWebKitInitialized();
+        }
     }
+    /* END: modbyme */
 }
