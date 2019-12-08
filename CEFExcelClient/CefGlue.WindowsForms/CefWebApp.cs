318e447c5e93617e76f25eda65752e094311d8cb
Rex <im@rex.tw>
2019-12-08 17:58:59 +0800

    調整程式碼

--- CEFExcelClient/CefGlue.WindowsForms/CefWebApp.cs
+++ CEFExcelClient/CefGlue.WindowsForms/CefWebApp.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace Xilium.CefGlue.WindowsForms
 {
-    class CefWebApp
+    /* BEG: modbyme */
+    public class CefWebApp : CefApp
     {
+        private readonly CefWebBrowserProcessHandler _browserProcessHandler;
+        private readonly CefWebRenderProcessHandler _renderProcessHandler;
+
+        public CefWebApp(CefWebBrowser browser)
+        {
+            _browserProcessHandler = new CefWebBrowserProcessHandler(browser);
+            _renderProcessHandler = new CefWebRenderProcessHandler(browser);
+        }
+
+        protected override CefBrowserProcessHandler GetBrowserProcessHandler()
+        {
+            return _browserProcessHandler;
+        }
+
+        protected override CefRenderProcessHandler GetRenderProcessHandler()
+        {
+            return _renderProcessHandler;
+        }
+
+        protected override CefResourceBundleHandler GetResourceBundleHandler()
+        {
+            return base.GetResourceBundleHandler();
+        }
+
+        protected override void OnBeforeCommandLineProcessing(string processType, CefCommandLine commandLine)
+        {
+            base.OnBeforeCommandLineProcessing(processType, commandLine);
+        }
+
+        protected override void OnRegisterCustomSchemes(CefSchemeRegistrar registrar)
+        {
+            base.OnRegisterCustomSchemes(registrar);
+        }
     }
+    /* END: modbyme */
 }
