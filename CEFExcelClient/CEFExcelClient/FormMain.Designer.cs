2676199910f7c93bb6b7aa620ea2cf8f92d823ff
Rex <im@rex.tw>
2019-12-08 18:37:00 +0800

    1_拖拉CefWebBrowser控制項 2_把CefWebBrowser控制項大小調整成和FormMain差不多大小 3_修改控制項StartUrl屬性,設定成http://tw.yahoo.com

--- CEFExcelClient/CEFExcelClient/FormMain.Designer.cs
+++ CEFExcelClient/CEFExcelClient/FormMain.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cefWebBrowser1 = new Xilium.CefGlue.WindowsForms.CefWebBrowser();
+            this.SuspendLayout();
+            // 
+            // cefWebBrowser1
+            // 
+            this.cefWebBrowser1.BrowserSettings = null;
+            this.cefWebBrowser1.Location = new System.Drawing.Point(-1, 1);
+            this.cefWebBrowser1.Name = "cefWebBrowser1";
+            this.cefWebBrowser1.Size = new System.Drawing.Size(798, 451);
+            this.cefWebBrowser1.StartUrl = "http://tw.yahoo.com";
+            this.cefWebBrowser1.TabIndex = 0;
+            this.cefWebBrowser1.Text = "cefWebBrowser1";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cefWebBrowser1);
+            this.Name = "FormMain";
             this.Text = "FormMain";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Xilium.CefGlue.WindowsForms.CefWebBrowser cefWebBrowser1;
     }
 }
