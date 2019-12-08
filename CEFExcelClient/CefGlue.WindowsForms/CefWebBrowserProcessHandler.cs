9b72f3e26911f77ceea358ac6ca898916835434f
Rex <im@rex.tw>
2019-12-08 18:01:53 +0800

    調整程式碼

--- CEFExcelClient/CefGlue.WindowsForms/CefWebBrowserProcessHandler.cs
+++ CEFExcelClient/CefGlue.WindowsForms/CefWebBrowserProcessHandler.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace Xilium.CefGlue.WindowsForms
 {
-    class CefWebBrowserProcessHandler
+    /* BEG: modbyme */
+    internal sealed class CefWebBrowserProcessHandler : CefBrowserProcessHandler
     {
+        private readonly CefWebBrowser _core;
+
+        public CefWebBrowserProcessHandler(CefWebBrowser core)
+        {
+            _core = core;
+        }
+
+        protected override CefPrintHandler GetPrintHandler()
+        {
+            return base.GetPrintHandler();
+        }
+
+        protected override void OnBeforeChildProcessLaunch(CefCommandLine commandLine)
+        {
+            base.OnBeforeChildProcessLaunch(commandLine);
+        }
+
+        protected override void OnContextInitialized()
+        {
+            base.OnContextInitialized();
+        }
+
+        protected override void OnRenderProcessThreadCreated(CefListValue extraInfo)
+        {
+            base.OnRenderProcessThreadCreated(extraInfo);
+        }
+
+        protected override void OnScheduleMessagePumpWork(long delayMs)
+        {
+            base.OnScheduleMessagePumpWork(delayMs);
+        }
     }
+    /* END: modbyme */
 }
